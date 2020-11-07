a642dac66d094bbc53d5a22f535294661200edec
Hayden Scott <HaydenScott573@gmail.com>
2020-11-06 19:44:46 -0700

    structure, simple gui, README

--- homestead/hobbies/bread/breadHydration.py
+++ homestead/hobbies/bread/breadHydration.py
@@ -11,13 +11,11 @@ totalFlour = 0
 sourdoughFlag = input("Is this a sourdough recipe? Y/n: ")
 if ( sourdoughFlag.upper() == "Y"):
     totalWater += float(input("Input water amount in starter in grams: "))
-    # totalWater += sourdoughWater
     totalFlour += float(input("Input flour amount in starter in grams: "))
-    # totalFlour += sourdoughFlour
 totalWater += float(input("Input water amount in grams: "))
 totalFlour += float(input("Input flour amount in grams: "))
 
-hydrationPercent = ((totalWater/totalFlour)*100)
+hydrationPercent = ((totalWater/totalFlour)*100.0)
 
 print(f"total water is: {totalWater}")
 print(f"total flour is: {totalFlour}")
